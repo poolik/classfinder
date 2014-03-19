de4fcf1943180e2919ba16b4aa46b5ad9137957e
Tõnis Pool <tonis.pool@gmail.com>
2014-03-19 15:44:13 +0200

    Removed @NotNull

--- src/test/java/com/poolik/classfinder/otherTestClasses/ConcreteClass.java
+++ src/test/java/com/poolik/classfinder/otherTestClasses/ConcreteClass.java
@@ -1,12 +1,11 @@
 package com.poolik.classfinder.otherTestClasses;
 
 import com.poolik.classfinder.ClassFinderException;
-import com.sun.istack.internal.NotNull;
 
 @Deprecated
 public class ConcreteClass extends AbstractClass {
 
-  @NotNull
+  @Deprecated
   private String test = "test";
   public int anotherValue = 0;
 
